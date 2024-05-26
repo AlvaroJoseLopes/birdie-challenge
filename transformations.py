90dda9c4d5d050f559c8df7120a7ba118afbd510
AlvaroJoseLopes <alvarojoselopes@hotmail.com>
2024-05-26 20:10:46 -0300

    feature: change parameters keys for all the transformation (to the same in challenge description)

--- transformations.py
+++ transformations.py
@@ -1,49 +1,49 @@
-def delete_transformation(data: dict, key: str) -> dict:
+def delete_transformation(data: dict, key1: str) -> dict:
     """
     Deletes the key from the dictionary
 
         Parameters:
             data (dict): target dictionary
-            key (str): key to be deleted
+            key1 (str): key to be deleted
 
         Returns:
             data (dict): target dictionary after applying `deletion` transformation.
     """
-    data.pop(key, None)
+    data.pop(key1, None)
     return data
 
 
-def set_transformation(data: dict, key: str, value: str) -> dict:
+def set_transformation(data: dict, key1: str, value1: str) -> dict:
     """
     Sets the value for a key in the dictionary
 
         Parameters:
             data (dict): target dictionary
-            key (str): target key
-            value (str): value to store
+            key1 (str): target key
+            value1 (str): value to store
 
         Returns:
             data (dict): target dictionary after applying `set` tranformation.
     """
-    data[key] = value
+    data[key1] = value1
     return data
 
 
-def rename_transformation(data: dict, old_key: str, new_key: str) -> dict:
+def rename_transformation(data: dict, key1: str, key2: str) -> dict:
     """
     Renames the `old_key`to `new_key` in the dictionary
 
         Parameters:
             data (dict): target dictionary
-            old_key (str): previous key to be renamed
-            new_key (str): renamed key
+            key1 (str): previous key to be renamed
+            key2 (str): renamed key
 
         Returns:
             data (dict): target dictionary after applying `rename` tranformation.
     """
-    current_value = data.get(old_key, None)
+    current_value = data.get(key1, None)
     if current_value is not None:
-        data = set_transformation(data, new_key, current_value)
-        data = delete_transformation(data, old_key)
+        data = set_transformation(data, key2, current_value)
+        data = delete_transformation(data, key1)
 
     return data
